4f9d3774490acc3080fe69171cc51de28ac0fa2b
ESV43 <142923756+ESV43@users.noreply.github.com>
2025-06-17 23:09:45 +0530

    Update StoryInputForm.tsx

--- components/StoryInputForm.tsx
+++ components/StoryInputForm.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { StoryInputOptions, ComicStyle, ComicEra, AspectRatio, GenerationProgress, ImageGenerationModel, TextGenerationModel, CaptionPlacement } from '../types';
 import { 
